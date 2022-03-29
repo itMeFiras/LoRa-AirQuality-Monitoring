e79bf930d8ef0a0bf568ae48d657e7a1e81f4f62
itMeFiras <47608098+itMeFiras@users.noreply.github.com>
2022-03-29 11:28:32 +0100

    user managment

--- Backend/routes/Users.js
+++ Backend/routes/Users.js
@@ -3,10 +3,9 @@ const User = require("../models/User");
 const bcrypt = require('bcrypt');
 const jwt = require ("jsonwebtoken");
 var nodemailer = require('nodemailer');
-var email = require("../routes/email");
 require('dotenv').config();
 
-// confirmation
+//confirmation
 router.get("/confirmation/:id",async (req,res)=>{
     const id = req.params.id
     User.findById(id).then((user)=>{
@@ -19,8 +18,18 @@ router.get("/confirmation/:id",async (req,res)=>{
     }).catch((err)=>{
         res.send({message: err.message})
     })
-
 })
+
+//random password
+function randompass(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
 
 //register
 router.post("/register",async (req,res)=>{
@@ -45,11 +54,12 @@ router.post("/register",async (req,res)=>{
               pass: process.env.sendpass
             }
         });
+
         var url = `http://localhost:8800/api/users/confirmation/${newUser._id}`
         var mailverif = {
             from: process.env.sendMail,
             to: req.body.email,
-            subject: 'Sending Email using Node.js',
+            subject: 'account activation',
             text: `please confirm: ${url}`
         };
           
@@ -60,7 +70,6 @@ router.post("/register",async (req,res)=>{
               console.log('Email sent: ' + info.response);
             }
         });
-        
 
         //save and send response
         const user = await newUser.save();
@@ -77,23 +86,159 @@ router.post("/login",async (req,res)=>{
 
         //find user
         const user = await User.findOne({username:req.body.username});
-        !user && res.status(400).json("wrong user/pass")
+        if (!user) return res.status(400).json("wrong user/pass")
         
         //validate password
         const validPassword = await bcrypt.compare(
             req.body.password,
             user.password);
-        !validPassword && res.status(400).json("wrong user/pass")
+        if (!validPassword) return res.status(400).json("wrong user/pass")
 
+        //check active
+        if(user.active == "active"){
         //jwt authenticate
         const accessToken = jwt.sign(user.username, process.env.Access_ts);
 
         //send res
-        res.status(200).json({_id:user._id, username:user.username, accessToken: accessToken});
+        return res.status(200).json({_id:user._id, username:user.username, accessToken: accessToken});
+        }
+        else return res.status(400).json("this user is not active")
 
     }catch(err){
-        res.status(500).json(err)   
+        return res.status(500).json(err)   
     }
+});
+
+//edit profile
+router.post("/editprofile/:id",async (req,res)=>{
+    const id = req.params.id
+
+    //generate the new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.password,salt)
+
+    //edit 
+    User.findById(id).then((user)=>{
+        user.username = req.body.username;
+        user.email = req.body.email;
+        user.password = hashedpassword;
+
+        user.save().then(()=>{
+            return res.send({message: 'edit success'})
+        }).catch((err)=>{
+            return res.send({message: err.message})
+        })
+    }).catch((err)=>{
+        return res.send({message: err.message})
+    })
+})
+
+
+//edit profile with a password check
+router.post("/editprofilecheck/:id",async (req,res)=>{
+    const id = req.params.id
+
+    //generate the new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.newpass,salt)
+
+    //find user
+    const userr = await User.findById(id);
+
+    //check old password
+    const checkPassword = await bcrypt.compare(
+        req.body.password,
+        userr.password);
+    if (!checkPassword) return res.status(400).json("the old password is wrong !")
+
+    //edit
+    User.findById(id).then((user)=>{
+
+        user.username = req.body.username;
+        user.email = req.body.email;
+        user.password = hashedpassword;
+
+        user.save().then(()=>{
+            return res.send({message: 'edit success'})
+        }).catch((err)=>{
+            return res.send({message: err.message})
+        })
+    }).catch((err)=>{
+        return res.send({message: err.message})
+    })
+})
+
+//delete profile
+router.delete("/deleteprofile/:id",async (req,res)=>{
+    const id = req.params.id
+    User.findByIdAndDelete(id).then(()=>{
+        return res.send({message: 'deleted'})
+    }).catch((err)=>{
+        return res.send({message: err.message})
+    })
+})
+
+//reset password
+router.post("/resetpass",async (req,res)=>{
+    try{
+
+        //find user
+        var user = await User.findOne({$and:[{username:req.body.username},{email:req.body.email}]});
+        if (!user) return res.status(400).json("wrong informations")
+
+        //set new password
+        var newpass = randompass(6);
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(newpass,salt)
+        user.password = hashedpassword;
+
+        //send mail
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: process.env.sendMail,
+              pass: process.env.sendpass
+            }
+        });
+
+        var mailverif = {
+            from: process.env.sendMail,
+            to: req.body.email,
+            subject: 'Password reset',
+            text: `your new password is: ${newpass}`
+        };
+          
+        transporter.sendMail(mailverif, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+        });
+
+        //save and send res
+        user.save().then(()=>{
+            return res.status(200).send({message: `your new password is: ${newpass}`})})
+    }catch(err){
+        return res.status(500).json(err)   
+    }
+});
+
+//get user list
+router.get("/list",async (req,res)=>{
+    User.find(function(err, todos) {
+        if (err) {
+            return res.status(500).json(err);
+        } else {
+            return res.json(todos);
+        }
+    });
+    /*try{
+        const user = await User.findall();
+        return res.status(200).json(user);
+    }catch(err){
+        return res.status(500).json(err)   
+    }*/
 });
 
 
